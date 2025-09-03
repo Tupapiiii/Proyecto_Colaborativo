0511f2a0ebf48bdae22d232df4adfc32f0a0b72b
Alvin Gómez <alvto2005@gmail.com>
2025-09-02 19:01:10 -0600

    Primer commit

---
+++ Quiz_Colaborativo/src/quiz_colaborativo/Quiz_Colaborativo.java
@@ -0,0 +1,21 @@
+
+package quiz_colaborativo;
+
+/**
+ *
+ * @author alvin
+ */
+public class Quiz_Colaborativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+           System.out.println("Hola Mundo");
+    }
+    
+    
+    
+}
